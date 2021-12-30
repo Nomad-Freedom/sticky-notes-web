f2d8cb49d95cc4a980e06574a7ef7d2396fcfa8e
Benny Hernandez <mail@bennyhernandez.com>
2021-12-30 18:20:55 -0500

    Feature: added date, time, and color to note component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,22 +5,55 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import { Note } from "../interfaces";
+import {
+  getTime,
+  isToday,
+  isYesterday,
+  getDateAndTime,
+} from "../utils/dateTime";
 
 interface NoteProps {
   data: Note;
 }
 
-function Note({ data: { title, description, id } }: NoteProps) {
+function Note({
+  data: { title, description, id, color, created_at },
+}: NoteProps) {
+  const today = isToday(created_at);
+  const yesterday = isYesterday(created_at);
+  const otherDay = today === false && yesterday === false;
+
   return (
-    <Card sx={{ width: "100%" }}>
+    <Card sx={{ width: "100%", backgroundColor: `${color}` }}>
       <CardContent>
+        {today && (
+          <Typography align="right" variant="caption" component={"p"}>
+            today @ {getTime(created_at)}
+          </Typography>
+        )}
+        {yesterday && (
+          <Typography align="right" variant="caption" component={"p"}>
+            yesterday @ {getTime(created_at)}
+          </Typography>
+        )}
+        {otherDay && (
+          <Typography align="right" variant="caption" component={"p"}>
+            {getDateAndTime(created_at)}
+          </Typography>
+        )}
+
         <Typography
-          sx={{ fontSize: 16, fontWeight: "bold" }}
+          sx={{
+            fontSize: 16,
+            fontWeight: "bold",
+            borderBottom: "1px solid rgba(0, 0, 0, .2)",
+          }}
           color="text.primary"
           gutterBottom
         >
           {title}
         </Typography>
+
         <Typography variant="body2">{description}</Typography>
       </CardContent>
     </Card>
