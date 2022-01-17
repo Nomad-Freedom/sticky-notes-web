d0b4e7b9a5749860c83e0bfb35d812837867f566
Benny Hernandez <mail@bennyhernandez.com>
2022-01-17 07:15:21 -0500

    Refactor: added fixes to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,13 +4,20 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
+import { useRouter } from "next/router";
 
 export default function () {
+  const { push } = useRouter();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ boxShadow: "none" }}>
         <Toolbar disableGutters>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="h2"
+            sx={{ cursor: "pointer" }}
+            onClick={() => push("/")}
+          >
             Sticky Notes
           </Typography>
 
@@ -19,6 +26,7 @@ export default function () {
             edge="start"
             color="inherit"
             aria-label="add note"
+            sx={{ marginLeft: "auto" }}
           >
             <AddIcon />
           </IconButton>
